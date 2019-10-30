28613930ba1db8a4261ca72fb88093b709d86cf7
Nick <eshnil2000@gmail.com>
2019-10-29 19:26:42 -0700

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-NEW_UUID=$($(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1))
-echo $NEW_UUID
+NEW_UUID=`(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)`
+echo "$NEW_UUID"
