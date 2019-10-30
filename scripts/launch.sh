f0daf29b67d71c7fb332c59ae6bcf554fc0b0124
Nick <eshnil2000@gmail.com>
2019-10-29 19:16:13 -0700

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-export NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo $NEW_UUID
 sleep 1
 echo $! > .pidfile
